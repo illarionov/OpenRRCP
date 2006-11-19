0132ce476c737dd672984ad56af85ab42d4ee836
Andrey "nording" Chernyak <andrew.chernyak@gmail.com>
2006-11-19 17:29:58 +0000

    Fix typo in source files banner

--- src/rrcp_config.c
+++ src/rrcp_config.c
@@ -1,18 +1,18 @@
 /*
     This file is part of openrrcp
 
-    pcapsipdump is free software; you can redistribute it and/or modify
+    OpenRRCP is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 
-    pcapsipdump is distributed in the hope that it will be useful,
+    OpenRRCP is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar; if not, write to the Free Software
+    along with OpenRRCP; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
     ---
